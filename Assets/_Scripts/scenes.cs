edab1c50da96c3627b64795db973e852d36848f7
MorganGrooms <54331560+MorganGrooms@users.noreply.github.com>
2019-10-26 23:54:49 -0500

    Add files via upload

---
+++ Assets/_Scripts/scenes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class scenes : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+
+
+
+    // Update is called once per frame
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
